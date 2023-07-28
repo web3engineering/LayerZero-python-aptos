ec11a8b23a7ae02cb7c9916a6fbe941874774b81
Ivan Kapeykin <5349983@gmail.com>
2023-07-28 09:58:57 +0300

    Implement str params on Aptos Node estimate gas method

--- src/sdk/aptos_sdk.py
+++ src/sdk/aptos_sdk.py
@@ -4,10 +4,8 @@ from aptos_sdk.authenticator import Authenticator, Ed25519Authenticator
 from aptos_sdk.client import RestClient
 from aptos_sdk.transactions import EntryFunction, TransactionPayload, RawTransaction, SignedTransaction
 from aptos_sdk.client import ApiError
-
 from src.const import APTOS_RPC
 from src.sdk.base_sdk import BaseSdk
-
 from typing import Any, Dict
 
 
@@ -17,26 +15,32 @@ class AptosSdk(BaseSdk):
         self.client = RestClient(APTOS_RPC)
 
     def send_and_submit_transaction(self, payload: EntryFunction) -> str:
-        GAS_SAFETY_FACTOR = 0.2 # 20%
+        GAS_SAFETY_FACTOR = 0.2  # 20%
         raw_transaction = self.client.create_bcs_transaction(
             self.account, TransactionPayload(payload)
         )
         simulated_transaction = self.simulate_transaction_with_gas_estimate(
             transaction=raw_transaction,
-            sender=self.account
         )
+
         if not simulated_transaction[0]["success"]:  # noqa
             raise ValueError(simulated_transaction[0]["vm_status"])  # noqa
-        self.client.client_config.max_gas_amount = int(int(simulated_transaction[0]['gas_used'], base=10) * (1 + GAS_SAFETY_FACTOR))
-        signed_transaction = self.client.create_bcs_signed_transaction(self.account, TransactionPayload(payload)
+
+        self.client.client_config.max_gas_amount = int(
+            int(simulated_transaction[0]['gas_used'], base=10) * (1 + GAS_SAFETY_FACTOR)
+        )
+
+        signed_transaction = self.client.create_bcs_signed_transaction(
+            self.account, TransactionPayload(payload)
+        )
         txn = self.client.submit_bcs_transaction(signed_transaction)
         self.client.wait_for_transaction(txn)
         return txn
 
     def simulate_transaction_with_gas_estimate(
-    self,
-        transaction: RawTransaction
-        ) -> Dict[str, Any]:
+            self,
+            transaction: RawTransaction
+    ) -> Dict[str, Any]:
         authenticator = Authenticator(
             Ed25519Authenticator(
                 self.account.public_key(),
@@ -44,18 +48,20 @@ class AptosSdk(BaseSdk):
             )
         )
         signed_transaction = SignedTransaction(transaction, authenticator)
-        params = {'estimate_gas_unit_price': True,
-                  'estimate_max_gas_amount': True}
-        headers = {"Content-Type": "application/x.aptos.signed_transaction+bcs"}
+        params = {
+            'estimate_gas_unit_price': "true",
+            'estimate_max_gas_amount': "true"
+        }
+        headers = {
+            "Content-Type": "application/x.aptos.signed_transaction+bcs"
+        }
         response = self.client.client.post(
             f"{self.client.base_url}/transactions/simulate",
             headers=headers,
-            content=signed_transaction.bytes(),
+            data=signed_transaction.bytes(),
             params=params
         )
         if response.status_code >= 400:
             raise ApiError(response.text, response.status_code)
 
         return response.json()
-
-    
